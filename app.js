6bb6f6a1aec709a8132df3ecfcfee3fa6748fe4d
Felipe Vieira <me@felipet.io>
2019-06-26 21:35:00 -0300

    Added routes and a basic template and style engine

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-// Copyright 2018, Google LLC.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var lessMiddleware = require('less-middleware');
+var logger = require('morgan');
 
-'use strict';
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const express = require('express');
+var app = express();
 
-const app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("*", function(req, res, next){
   if (req.hostname != 'localhost' && req.get('X-Forwarded-Proto') == 'http') {
@@ -26,13 +30,23 @@ app.use("*", function(req, res, next){
   }
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-if (module === require.main) {
-  const server = app.listen(process.env.PORT || 8080, () => {
-    const port = server.address().port;
-    console.log(`App listening on port ${port}`);
-  });
-}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
